988c3fba86fba21a14b49068d8630906f715b9e7
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2021-05-05 09:35:36 +0100

    refactor: put exporters behind feature flag (#1351)

--- observability_deps/src/lib.rs
+++ observability_deps/src/lib.rs
@@ -3,13 +3,20 @@
 //! single crate.
 
 // Export these crates publicly so we can have a single reference
-pub use env_logger;
-pub use opentelemetry;
-pub use opentelemetry_jaeger;
-pub use opentelemetry_otlp;
-pub use opentelemetry_prometheus;
-pub use prometheus;
 pub use tracing;
-pub use tracing::instrument;
-pub use tracing_opentelemetry;
-pub use tracing_subscriber;
+pub use opentelemetry;
+
+#[cfg(feature = "exporters")]
+mod exporters {
+    pub use tracing::instrument;
+    pub use tracing_opentelemetry;
+    pub use tracing_subscriber;
+    pub use env_logger;
+    pub use opentelemetry_jaeger;
+    pub use opentelemetry_otlp;
+    pub use opentelemetry_prometheus;
+    pub use prometheus;
+}
+
+#[cfg(feature = "exporters")]
+pub use exporters::*;
